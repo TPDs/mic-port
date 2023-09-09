f334c8f08ae28a9551daafe214d936c8f6e58ae3
TPDs <mig.mic@gmail.com>
2023-09-09 14:48:19 +0200

    bun test

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-node'
+//import adapter from "svelte-adapter-bun";
 import dotenv from 'dotenv';
 
 dotenv.config();
