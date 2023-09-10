506d2903fdb3aa0d1cded5a8b4270d69d1899946
TPDs <mig.mic@gmail.com>
2023-09-10 13:59:53 +0200

    push test

--- src/routes/projectlist/+page.svelte
+++ src/routes/projectlist/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../styles.css';
+	import '../styles.css';	
 	import avatar from '$lib/images/avatar.png';
 </script>
 
@@ -11,14 +11,35 @@
 <br /><br /><br />
 
 <aboutsection>
-	<h1>Projects</h1>
-	
-	First
-	Second
+	<h1>Who am I</h1>
+	<avatar-img>
+		<img src={avatar} alt="Avatar" />
+	</avatar-img>
+	<about-txt>
+		My name is Michael Petersen and im a 36 year old man living in Copenhagen Denmark, with a flair for coding and all it wonders. <br
+		/>
+		I made this site to try and expose some of the areas that I want to learn and show others
+		<br /><br />
+		
+		My Background: 
 
+		<br /><br /><br />
+		2022-2023 &nbsp &nbsp - &nbsp &nbsp
+		<a href="https://kompetence.kea.dk/uddannelser/it/diplom-i-it-sikkerhed"
+			>Bachelor in It-Security, KEA Guldbergsgade
+		</a> <br />
+		2019-2022 &nbsp &nbsp - &nbsp &nbsp
+		<a href="https://kea.dk/uddannelser/erhvervsakademi/datamatiker">
+			Datascience, KEA Lygten
+		</a> <br />
+		2017-2019 &nbsp &nbsp - &nbsp &nbsp HF, HF-Centret Efterslægten <br />
+		<br /><br />
+
+		**Work in Progress**
+	</about-txt>
 
 	<date-txt>
-		<h3>10.Sep 2023 - Michael</h3>
+		<h3>5.Sep 2023 - Michael</h3>
 	</date-txt>
 </aboutsection>
 
