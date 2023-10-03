8ccc1fc9518ca84f7e903d784ea48c6235036347
TPDs <mig.mic@gmail.com>
2023-10-03 13:36:08 +0200

    added text to Job

--- src/routes/jobpage/+page.svelte
+++ src/routes/jobpage/+page.svelte
@@ -11,8 +11,9 @@
 
 <mainsection>
 	<h1mp>Job</h1mp>
-	<about-txt>Test 4</about-txt>
-	<date-txt>18.Sep 2023 - Michael</date-txt>
+	<about-txt> <br> <br><br><br>I am currently looking for a job <br> <br> If your company have an opening , don´t hesitate to contact me via LinkedIn or Github
+	<br>I am also open for konsulten jobs, if you need a API or a system setup</about-txt>
+	<date-txt>03.Okt 2023 - Michael</date-txt>
 </mainsection>
 
 <style>
