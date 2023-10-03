0a3669ffc94a1f6d9dab8a8d8d27f90e81178d07
TPDs <mig.mic@gmail.com>
2023-10-03 16:43:11 +0200

    github changes

--- src/routes/projectlist/+page.server.ts
+++ src/routes/projectlist/+page.server.ts
@@ -1,5 +1,33 @@
 import type { PageServerLoad } from './$types';
+import type { PageLoad } from './$types';
+import { PUBLIC_GITHUB } from '$env/static/public'
+import { Octokit } from "octokit";
+import { createTokenAuth } from "@octokit/auth-token";
 
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
+export const load: PageLoad = async ({ params }) => {
+
+    const octokit = new Octokit({
+        auth: PUBLIC_GITHUB
+    })
+
+    await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+        owner: 'TPDs',
+        repo: 'mp_Python1',
+        path: 'README.md',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+    console.log(octokit);
+
+
+
+
+    return {
+        data: [{ rep: 'rep1', readme: 'dette er readme1', link: '/' }, { rep: 'rep1', readme: 'dette er readme2', link: '/' }, { rep: 'rep1', readme: 'dette er readme3', link: '/' }]
+    };
+};
+
+
+
+
--- src/routes/projectlist/+page.ts
+++ src/routes/projectlist/+page.ts
@@ -1,15 +1,5 @@
-
 import type { PageLoad } from './$types';
 
-
-export const prerender = true;
-
-export const load: PageLoad = ({ params }) => {
-	return {
-        data: [ {rep: 'rep1', readme: 'dette er readme1', link :'/'}, {rep: 'rep1', readme: 'dette er readme2', link :'/'}, {rep: 'rep1', readme: 'dette er readme3', link :'/'} ]
-	};
-};
-
-
-
-
+export const load = (async () => {
+    return {};
+}) satisfies PageLoad;
