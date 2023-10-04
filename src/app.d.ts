44f3e513603a8dad807650cad18bbf6620682b02
TPDs <mig.mic@gmail.com>
2023-10-04 12:53:04 +0200

    test

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,10 @@
 
-import type {Pocketbase} from "pocketbase"
+//import type {Pocketbase} from "pocketbase"
 
 declare global {
 	namespace App {
 		// interface Error {}
-		interface Locals {pb: Pocketbase}
+		//interface Locals {pb: Pocketbase}
 		// interface PageData {}
 		// interface Platform {}
 	}
