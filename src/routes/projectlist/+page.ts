a378bb303d91211382996c711a2e568679836f10
TPDs <mig.mic@gmail.com>
2023-10-05 14:55:00 +0200

    last

--- src/routes/projectlist/+page.ts
+++ src/routes/projectlist/+page.ts
@@ -1,6 +1,8 @@
 import type { PageLoad } from './$types';
 import {pb} from '$lib/tools/pb'
 
+export const prerender = false;
+
 export const load: PageLoad = async ({  })=> {     
        
         const records = await pb.collection('project').getFullList({
