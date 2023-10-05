d9248ed2318842c136868ac6a918ed22b8e9134d
TPDs <mig.mic@gmail.com>
2023-10-05 13:42:20 +0200

    v2

--- src/lib/tools/pb.ts
+++ src/lib/tools/pb.ts
@@ -2,10 +2,4 @@ import PocketBase from 'pocketbase';
 import { env } from '$env/dynamic/public';
 
 export const pb = new PocketBase(env.PUBLIC_PB_URL);
-export const authData = await pb.admins.authWithPassword(env.PUBLIC_PB_ADMIN, env.PUBLIC_PB_PW);
-const records = await pb.collection('project').getFullList({
-  sort: '-created',
-});
-
-
-
+export const authData =  pb.admins.authWithPassword(env.PUBLIC_PB_ADMIN, env.PUBLIC_PB_PW);
