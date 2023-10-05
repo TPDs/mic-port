066f6ba55c5a3174330438039f7b4925e886d359
TPDs <mig.mic@gmail.com>
2023-10-05 14:14:52 +0200

    tweak

--- src/lib/tools/pb.ts
+++ src/lib/tools/pb.ts
@@ -2,4 +2,5 @@ import PocketBase from 'pocketbase';
 import { env } from '$env/dynamic/public';
 
 export const pb = new PocketBase(env.PUBLIC_PB_URL);
-export const authData =  pb.admins.authWithPassword(env.PUBLIC_PB_ADMIN, env.PUBLIC_PB_PW);
+export const authData =  pb.admins.authWithPassword(env.PUBLIC_PB_ADMIN as string , env.PUBLIC_PB_PW as string);
+
