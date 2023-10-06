80bc9a39bf85521c04a227fa243ed1262c0880ff
TPDs <mig.mic@gmail.com>
2023-10-06 14:40:22 +0200

    test2

--- server.js
+++ server.js
@@ -4,16 +4,8 @@ import dotenv from 'dotenv';
 import https from 'https';
 import express from "express"
 import { handler } from './build/handler.js';
-import path from 'path';
 
 dotenv.config();
-
-const envFile = path.resolve(__dirname, '.env');
-const envConfig = dotenv.parse(fs.readFileSync(envFile));
-
-for (const key in envConfig) {
-  process.env[key] = envConfig[key];
-}
 
 const sslkey = fs.readFileSync(env.key)
 const sslcert = fs.readFileSync(env.cert)
