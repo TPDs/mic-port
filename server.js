afe129b4a4e095cdbdb974bdf49cf45b6aeab7cd
TPDs <mig.mic@gmail.com>
2023-10-07 00:48:34 +0200

    minor

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 import fs from 'fs';
-import { env } from '$env/dynamic/public';
+import { env } from 'process';
 import dotenv from 'dotenv';
 import https from 'https';
 import express from "express"
 import { handler } from './build/handler.js';
 
-
-dotenv.config('');
-
-console.log(env.PUBLIC_PB_URL);
+dotenv.config();
 
 const sslkey = fs.readFileSync(env.key)
 const sslcert = fs.readFileSync(env.cert)
