6e7d7297207cd72a4434c7ab7b6fdeae6d3ae6a8
TPDs <mig.mic@gmail.com>
2023-10-07 13:03:09 +0200

    test

--- src/lib/tools/pb.ts
+++ src/lib/tools/pb.ts
@@ -1,6 +1,8 @@
 import PocketBase from 'pocketbase';
 import { env } from '$env/dynamic/public';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 export const pb = new PocketBase(env.PUBLIC_PB_URL);
 export const authData =  pb.admins.authWithPassword(env.PUBLIC_PB_ADMIN as string , env.PUBLIC_PB_PW as string);
