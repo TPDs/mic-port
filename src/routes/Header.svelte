298621d787b4fde8c79dfe76ce4d684b14de0ffc
TPD <mig.mic@gmail.com>
2023-11-02 14:21:12 +0100

    done for now

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -28,7 +28,7 @@
 			<nav>
 				<ul2>
 					<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-						<b>TP code</b>
+						<b>TPcoded</b>
 					</li>
 				</ul2>
 			</nav>
@@ -144,27 +144,26 @@
 		box-shadow: 0em 0em 0.2em;
 		/* text-shadow: -1rem 6rem 0rem var(--color-box-hover); */
 	}
-	nav b {
-		font-size: 1.5rem;
-		font-weight: 700;
-		letter-spacing: 0.1rem;
-		text-transform: uppercase;		
-		text-align: center;
-		margin: auto;
-		white-space: nowrap;		
-		padding-right: 1rem;
-	}
 
-	nav b:before {
-		background-color: var(--color-text-hover);
-		content: '';
-		display: block;			
-		height: 3px;
-		width:7.4rem;
-		border: rgba(43, 40, 40,0.7);
-		border-radius: 0.5em;
-		border-style: solid;
-		
+
+	nav b {
+		@supports (background-clip: text) or (-webkit-background-clip: text) {
+			background-image: url("data:image/svg+xml,%3Csvg width='2250' height='900' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Cpath fill='%2300A080' d='M0 0h2255v899H0z'/%3E%3Ccircle cx='366' cy='207' r='366' fill='%2300FDCF'/%3E%3Ccircle cx='177.5' cy='318.5' r='477.5' fill='%2300FDCF'/%3E%3Ccircle cx='1215' cy='737' r='366' fill='%23008060'/%3E%3C/g%3E%3C/svg%3E%0A");
+			background-size: 110% auto;
+			background-position: center;
+			color: var(--color-text);
+			opacity: 0.7;
+			-webkit-background-clip: text;
+			background-clip: text;
+			font-size: 3rem;
+			font-weight: 700;
+			letter-spacing: -0.4rem;
+			text-transform: uppercase;
+			text-align: center;
+			margin: auto;
+			white-space: nowrap;
+			padding-right: 1rem;
+		}
 	}
 
 	@media only screen and (max-width: 768px) {
