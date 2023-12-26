000ce8fd11352990b789cde3195036db44d94c3b
TPD <mig.mic@gmail.com>
2023-12-27 00:23:08 +0100

    Add ViewTransition component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>
+	import ViewTransition from './navigation.svelte'
 	import Header from './Header.svelte';
 	import './styles.css';
 	import '../app.postcss';
-	import Sidebar from './Sidebar.svelte';
+	import Sidebar from './Sidebar.svelte';	
 </script>
 
 <div class="app">
-	<Header />
-	
+	<ViewTransition/>
+	<Header />	
 	<sidebarstyle>
 		<Sidebar />
 	</sidebarstyle>
