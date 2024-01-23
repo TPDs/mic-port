9edb2a131f3a479a9554382f5dad370c643b256a
TPD <mig.mic@gmail.com>
2024-01-23 17:52:02 +0100

    Add prerender error handling in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,16 @@ const config = {
 			polyfill: false,
 			csrf: {
 				checkOrigin: false
-			}
+			}, prerender: {
+				handleHttpError: ({ path, referrer, message }) => {
+					// ignore deliberate link to shiny 404 page
+					if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+						return;
+					}
+
+					// otherwise fail the build
+					throw new Error(message);
+				}
 		})
 	}
 };
