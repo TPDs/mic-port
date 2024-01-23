a91e717dbe7cea08aa16356e740d8a1282dea85b
TPD <mig.mic@gmail.com>
2024-01-23 17:25:57 +0100

    Fix load function in projectlist page server

--- src/routes/projectlist/+page.server.ts
+++ src/routes/projectlist/+page.server.ts
@@ -1,6 +1,6 @@
 import type { PageServerLoad } from './$types';
 
-const prerender = false; // set to true to enable prerendering
+const prerender = true; // set to true to enable prerendering
 
 export const load: PageServerLoad = (async ({ locals }) => {
     const records = await locals.pb.collection('project').getFullList({
@@ -9,4 +9,4 @@ export const load: PageServerLoad = (async ({ locals }) => {
     console.log('records', records);
     return { records: records };
     
-})  satisfies PageServerLoad;
+})
