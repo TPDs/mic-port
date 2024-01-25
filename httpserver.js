0c66b4c53f68623fcd10be8c35e1998d5d5ae5bd
TPD <mig.mic@gmail.com>
2024-01-25 16:17:30 +0100

    Commented out unused import statement and console.log statement

--- httpserver.js
+++ httpserver.js
@@ -2,11 +2,9 @@ import dotenv from 'dotenv';
 import http from 'http';
 import express from "express"
 import { handler } from './build/handler.js';
-import { env } from '$env/dynamic/public';
+//import { env } from '$env/dynamic/public';
 
 dotenv.config();
-
-console.log(env.PUBLIC_PB_URL);
 
 const app = express()
 app.use(handler)
